0f459c2f87d9f228f06a7b98c8e42c1f48fcbbb7
alexanderpo <alexander.pozhidaev@gmail.com>
2017-10-13 21:52:54 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,31 @@
+export const SYNC = null;
+export const FETCH = ['REQUEST', 'SUCCESS', 'FAILURE'];
+export const ASYNC = ['BEGIN', 'END'];
+
+
+export function action(type, payload = {}) {
+  return { type, ...payload };
+}
+
+export function createType(namespace, ...paths) {
+  const globalPath = paths.length ? `${paths.join('_')}_` : '';
+  const globalPrefix = namespace ? `${namespace}/${globalPath}` : globalPath;
+  const type = (definition, prefix) => {
+    const container = Object.keys(definition).reduce((ctr, key) => {
+      const value = definition[key];
+      if (Array.isArray(value)) {
+        ctr[key] = value.reduce((sufixs, sufix) => {
+          sufixs[sufix] = `${prefix}${key}_${sufix}`;
+          return sufixs;
+        }, {});
+      } else if (value && typeof value === 'object') {
+        ctr[key] = type(value, `${prefix}${key}_`);
+      } else {
+        ctr[key] = `${prefix}${key}`;
+      }
+      return ctr;
+    }, {});
+    return container;
+  };
+  return definition => type(definition, globalPrefix);
+}
